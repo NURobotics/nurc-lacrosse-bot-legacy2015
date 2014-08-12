5e85e96cb9d14c69b56a2d2f6855f691735e5792
tgdiriba <tgdiriba@gmail.com>
2014-08-12 09:19:38 -0500

    Added Tested Code

--- src/Ball2DTrackerTest.cpp
+++ src/Ball2DTrackerTest.cpp
@@ -12,9 +12,10 @@ int main(int argc, char **argv)
   cv::namedWindow("Transformed Image", CV_WINDOW_AUTOSIZE);
 
   if(response == 'y') {
-    nurc::Ball2DTracker tracker;
+    nurc::Ball2DTracker* tracker = new nurc::Ball2DTracker;
 
     while(cv::waitKey(1) == -1) {
+			/* tracker.captureImageFrame();
       cv::imshow("Feed Image", tracker.getImage());
       cv::imshow("Transformed Image", tracker.getTransformedImage());
 
@@ -22,8 +23,10 @@ int main(int argc, char **argv)
       cv::Point estimated_center = tracker.getEstimatedCenter();
 
       std::cout << "The measured centroid was calcualted at: (" << measured_center.x << ", " << measured_center.y << ")" << std::endl;
-      std::cout << "The filtered estimate was calculated at: (" << estimated_center.x << ", " << estimated_center.y << ")" << std::endl;
+      std::cout << "The filtered estimate was calculated at: (" << estimated_center.x << ", " << estimated_center.y << ")" << std::endl;*/
     }
+
+		delete tracker;
   }
 
   std::cout << std::endl << "Ball2DTracker main test completed." << std::endl;
