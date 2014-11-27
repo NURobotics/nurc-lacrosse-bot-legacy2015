6f5f5e0a81e6581997a78a04ddced14e1af8d7d8
Sean Yeh <sean.yeh@gmx.com>
2014-11-27 15:20:00 -0600

    Add CCL and some minor changes

--- scripts/Algorithm.py
+++ scripts/Algorithm.py
@@ -3,31 +3,49 @@
 import cv2
 import numpy as np
 
+import IPython
+
 class Algorithm(object):
   """General base class functionality for any Algorithm objects"""
   def __init__(self):
     pass
-  
-  def algorithm(self, i):
+
+  def algorithm(self, im):
     pass
-  
-  def __call__(self, i):
-    return self.algorithm(i)
+
+  def __call__(self, im):
+    return self.algorithm(im)
 
 
 class Centroid(Algorithm):
-  
+
   def __init__(self):
     super(Centroid, self).__init__()
-  
-  def algorithm(self, i):
+
+  def algorithm(self, im):
     """Calculate the centroid of an input image"""
-    if isinstance(i, np.ndarray) and len(i.shape) >= 2:
-      c = cv2.moments(i)
+    if isinstance(im, np.ndarray) and len(im.shape) >= 2:
+      c = cv2.moments(im)
       if abs(c['m00']) > 0.001:
         return (int(c['m10']/c['m00']), int(c['m01']/c['m00']))
       else:
-        return (0, 0)
+        return (-1, -1)
+
+
+class CCL(Algorithm):
+
+  def __init__(self):
+    super(CCL, self).__init__()
+
+  def algorithm(self, im):
+    if isinstance(im, np.ndarray) and len(im.shape) >= 2:
+      contours, hierarchy = cv2.findContours(im, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+      if len(contours) > 0:
+        max_contour = max(contours, key=len)
+        return tuple(np.mean(max_contour[:,0,:], axis=0).astype(int))
+      else:
+        return (-1, -1)
 
 if __name__ == '__main__':
   # Algorithm main test sequences
--- scripts/Ball2DTracker.py
+++ scripts/Ball2DTracker.py
@@ -4,17 +4,17 @@ import cv2
 import numpy as np
 from Tracker import Tracker
 from Filter import ThresholdFilter
-from Algorithm import Centroid
+from Algorithm import Centroid, CCL
 
 class Ball2DTracker(Tracker):
-  
-  def __init__(self):
+
+  def __init__(self, algorithm):
     super(Ball2DTracker, self).__init__()
     self.threshold_filter = ThresholdFilter( np.array([150, 50, 50], dtype=np.uint8), np.array([175, 255, 255], dtype=np.uint8) )
-    self.centroid_algo = Centroid()
+    self.algo = algorithm()
     self.transformed_image = np.copy( self.image )
-    self.centroid = self.centroid_algo(self.threshold_filter(self.transformed_image))
-  
+    self.center = (0, 0)
+
   def captureImageFrame(self):
     super(Ball2DTracker, self).captureImageFrame()
     if self.valid:
@@ -23,15 +23,21 @@ class Ball2DTracker(Tracker):
       self.transformed_image = cv2.GaussianBlur(self.transformed_image, (9,9), 2, self.transformed_image, 2)
       self.transformed_image = cv2.cvtColor(self.transformed_image, cv2.COLOR_BGR2HSV)
       self.transformed_image = self.threshold_filter(self.transformed_image)
-      self.centroid = self.centroid_algo(self.transformed_image)
+
+      # Calculate center
+      center = self.algo(self.transformed_image)
+      if center != (-1, -1):
+        self.center = tuple([2*i for i in center])
+
+      # Draw center
       self.transformed_image = cv2.cvtColor(self.transformed_image, cv2.cv.CV_GRAY2BGR)
-      self.centroid = tuple([2*i for i in self.centroid])
-      cv2.circle(self.transformed_image, self.centroid, 2, (255, 0, 0), -1)
-      cv2.circle(self.image, self.centroid, 2, (255, 0, 0), -1)
+      cv2.circle(self.transformed_image, self.center, 2, (255, 0, 0), -1)
+      cv2.circle(self.image, self.center, 2, (255, 0, 0), -1)
+
 
 if __name__ == '__main__':
   # Ball Tracker Tests
-  b2dt = Ball2DTracker()
+  b2dt = Ball2DTracker(CCL)
   while cv2.waitKey(1) == -1 and b2dt.valid:
     cv2.imshow("Transformed Image", b2dt.transformed_image)
     cv2.imshow("Original Image", b2dt.image)
