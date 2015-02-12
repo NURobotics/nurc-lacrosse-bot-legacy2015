c0769f914509331eecb60cd5e807d7f81d03e9b1
Tigist Diriba <tgdiriba@gmail.com>
2015-02-11 20:26:57 -0600

    Added python KF

--- scripts/Ball2DTracker.py
+++ scripts/Ball2DTracker.py
@@ -3,7 +3,7 @@
 import cv2
 import numpy as np
 from Tracker import Tracker
-from Filter import ThresholdFilter
+from Filter import ThresholdFilter, KalmanFilter
 from Algorithm import Centroid
 
 class Ball2DTracker(Tracker):
@@ -11,6 +11,11 @@ class Ball2DTracker(Tracker):
   def __init__(self):
     super(Ball2DTracker, self).__init__()
     self.threshold_filter = ThresholdFilter( np.array([150, 50, 50], dtype=np.uint8), np.array([175, 255, 255], dtype=np.uint8) )
+    
+    # Setup the Kalman Filter noise matrices
+    self.Q = (10**-3) * np.eye(4,4)
+    self.R = (10**-1) * np.eye(2,2)
+    self.kalman_filter = KalmanFilter(self.Q, self.R)
     self.centroid_algo = Centroid()
     self.transformed_image = np.copy( self.image )
     self.centroid = self.centroid_algo(self.threshold_filter(self.transformed_image))
@@ -26,6 +31,8 @@ class Ball2DTracker(Tracker):
       self.centroid = self.centroid_algo(self.transformed_image)
       self.transformed_image = cv2.cvtColor(self.transformed_image, cv2.cv.CV_GRAY2BGR)
       self.centroid = tuple([2*i for i in self.centroid])
+      filtered = self.kalman_filter(self.centroid)
+      self.centroid = (int(filtered[0,0]), int(filtered[1,0]))
       cv2.circle(self.transformed_image, self.centroid, 2, (255, 0, 0), -1)
       cv2.circle(self.image, self.centroid, 2, (255, 0, 0), -1)
 
--- scripts/Filter.py
+++ scripts/Filter.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import time
 
 
 class Filter(object):
@@ -40,9 +41,40 @@ class ThresholdFilter(Filter):
 
 class KalmanFilter(Filter):
   
-  def __init__(self):
-    pass
-
+  def __init__(self, Q, R):
+    super(KalmanFilter, self).__init__()
+    self.discrete_times = [0., time.time()]
+    self.discrete_positions = [(0., 0.), (0., 0.)]
+    dt = 0.
+    self.H = np.array([[1., 0., dt, 0.], [0., 1., 0., dt], [0., 0., 1., 0.], [0., 0., 0., 1.]], np.float32);
+    self.F = np.array([[1.0, 0., 0., 0.], [0., 1., 0., 0.]], np.float32);
+    
+    # Setup the noise matrices Q and R
+    if Q == None or R == None:
+      self.Q = (10**-3)*np.eye(4,4, np.float32)
+      self.R = (10**-1)*np.eye(2,2, np.float32)
+    else:
+      self.Q = Q.astype(np.float32)
+      self.R = R.astype(np.float32)
+    self.S = np.zeros([4,1], np.float32)
+    self.P = np.zeros([4,4], np.float32)
+  
+  def filter(self, point):
+    if type(point) == tuple or type(point) == list:
+      self.discrete_times[0] = self.discrete_times[1]
+      self.discrete_times[1] = time.time()
+      self.discrete_positions[0] = self.discrete_positions[1]
+      self.discrete_positions[1] = tuple(point)
+      velocity = (self.discrete_positions[1][0] - self.discrete_positions[0][0], self.discrete_positions[1][1] - self.discrete_positions[0][1])
+      dt = self.discrete_times[1] - self.discrete_times[0]
+      self.H[0,2] = self.H[1,3] = dt
+      self.S = self.H * self.S
+      self.P = self.H * self.P * np.transpose( self.H ) + self.Q
+      kalman_gain = np.dot(np.dot(self.P, np.transpose( self.F )), np.linalg.pinv( np.dot(np.dot(self.F, self.P), np.transpose( self.F )) + self.R ))
+      y = np.transpose( np.array([point[0], point[1], velocity[0], velocity[1]], np.float32) )
+      self.S = self.S - np.dot(kalman_gain, (y - np.dot(self.F,self.S) ))
+      self.P = np.dot((np.eye(4,4) - np.dot(kalman_gain, self.F)), self.P)
+    return self.S
 
 if __name__ == '__main__':
   # ThresholdFilter Main Tests
